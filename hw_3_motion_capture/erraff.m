90e69087313855227e161ae24cd62e54f7be6703
Mykola Morgunenko <Olli25010@gmail.com>
2022-11-11 23:12:15 +0100

    add hw 3: finished

--- hw_3_motion_capture/erraff.m
+++ hw_3_motion_capture/erraff.m
@@ -9,15 +9,17 @@ function d = erraff(A)
     % d: m-by-1 matrix
     %
     b0 = sum(A, 2) / size(A, 2);
-    [U, ~] = eig(A*A' - b0);
+    A = A - b0;
+    [U, ~] = eig(A*A.');
     d = zeros(size(U, 1), 1);
     size(d)
-    for k = 1:size(U, 1) - 1
+    for k = 1:size(U, 1)
         U_k = U(:, end-k+1:end);
-        C_k = U_k * U_k.' * A + b0;
-        d(k) = sum(vecnorm(C_k - A));
+        C_k = U_k * U_k.' * A;
+        C_k = C_k - A;
+        d(k) = sum(sum(C_k .* C_k));
     end
-
+    disp(d(end))
 end
 
 
